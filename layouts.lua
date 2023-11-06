9e7333743001487fadc5c84122cf09bb048883aa
thialfi17 <thialfi17@gmail.com>
2023-11-06 22:58:09 +0000

    Tidying layouts.lua

--- layouts.lua
+++ layouts.lua
@@ -19,8 +19,7 @@ local layouts = {
             r:set_gaps(config.gaps)
         end
 
-        local wins = r:populate(args.count, config)
-        return wins
+        return r:populate(args.count, config)
     end,
 
     stack = function(args, config)
@@ -28,8 +27,7 @@ local layouts = {
 
         r:set_gaps(config.gaps)
 
-        local wins = r:populate(args.count, config)
-        return wins
+        return r:populate(args.count, config)
     end,
 
     grid = function(args, config)
@@ -39,8 +37,7 @@ local layouts = {
             r:set_gaps(config.gaps)
         end
 
-        local wins = r:populate(args.count, config)
-        return wins
+        return r:populate(args.count, config)
     end,
 
     main_with_stack = function(args, config)
@@ -61,21 +58,25 @@ local layouts = {
         local main_width = math.floor(main_ratio * r.width)
 
         local main = r:from(0, 0, main_width, r.height):set_layout(main_layout, {main_count, main_count})
+
         r:next_tier()
+
         local sub = r:from(main_width, 0, r.width - main_width, r.height)
 
         if secondary_count ~= 0 then
             sub:set_layout(secondary_sublayout, {1, nil})
+
             local secondary_height = math.floor(secondary_ratio * sub.height)
             local secondary = sub:from(0, 0, sub.width, secondary_height):set_layout(secondary_sublayout, {secondary_count, secondary_count})
+
             r:next_tier()
+
             local remaining = sub:from(0, secondary_height, sub.width, sub.height - secondary_height):set_layout(tertiary_sublayout, {1, nil})
         else
             sub:set_layout(tertiary_sublayout, {1, nil})
         end
 
-        local wins = r:populate(args.count, config)
-        return wins
+        return r:populate(args.count, config)
     end,
 
     centred = function(args, config)
@@ -87,49 +88,53 @@ local layouts = {
         local secondary_sublayout = config.secondary_sublayout
         local tertiary_sublayout = config.tertiary_sublayout
 
-        local c = Region:from_args(args):set_layout(main_layout)
+        local r = Region:from_args(args):set_layout(main_layout)
 
         if not (config.smart_gaps and args.count < 2) then
-            c:set_gaps(config.gaps)
+            r:set_gaps(config.gaps)
         end
 
-        local main_offset = math.floor(c.width * (0.5 - main_ratio / 2))
-        local main_width =  math.floor(c.width * main_ratio)
+        local main_offset = math.floor(r.width * (0.5 - main_ratio / 2))
+        local main_width =  math.floor(r.width * main_ratio)
 
+        -- If not enough windows are present to totally fill the main region and the two side regions,
+        -- fill the main region with as much as we can while leaving two windows free to become the sides
         local main_max = main_count
         if args.count < main_count + 2 then
             main_max = args.count - 2
         end
-        local main = c:from(main_offset, 0, main_width, c.height):set_layout(main_layout, {1, main_max})
 
-        c:next_tier()
+        local main = r:from(main_offset, 0, main_width, r.height):set_layout(main_layout, {1, main_max})
+
+        r:next_tier()
 
         local left, right
 
         if secondary_count == 0 then
-            left = c:from(0, 0, main_offset, c.height):set_layout(tertiary_sublayout, {1, nil})
-            right = c:from(main_offset + main_width, 0, main_offset, c.height):set_layout(tertiary_sublayout, {1, nil})
+            left = r:from(0, 0, main_offset, r.height):set_layout(tertiary_sublayout, {1, nil})
+            right = r:from(main_offset + main_width, 0, main_offset, r.height):set_layout(tertiary_sublayout, {1, nil})
         else
-            left = c:from(0, 0, main_offset, c.height):set_layout(secondary_sublayout, {1, nil})
-            right = c:from(main_offset + main_width, 0, main_offset, c.height):set_layout(secondary_sublayout, {1, nil})
-        end
-
-        if secondary_count ~= 0 then
-            local secondary_height = math.floor(c.height * secondary_ratio)
+            local secondary_height = math.floor(r.height * secondary_ratio)
             local remaining_offset = secondary_height
-            local remaining_height = c.height - remaining_offset
+            local remaining_height = r.height - remaining_offset
+
+            left = r:from(0, 0, main_offset, r.height):set_layout(secondary_sublayout, {1, nil})
+            right = r:from(main_offset + main_width, 0, main_offset, r.height):set_layout(secondary_sublayout, {1, nil})
 
             local left_top = left:from(0, 0, main_offset, secondary_height):set_layout(secondary_sublayout, {secondary_count, secondary_count})
+
             left:next_tier()
+
             local left_btm = left:from(0, secondary_height, main_offset, remaining_height):set_layout(tertiary_sublayout)
 
             local right_top = right:from(0, 0, main_offset, secondary_height):set_layout(secondary_sublayout, {secondary_count, secondary_count})
+
             right:next_tier()
+
             local right_btm = right:from(0, secondary_height, main_offset, remaining_height):set_layout(tertiary_sublayout)
         end
 
-        local wins = c:populate(args.count, config)
-        return wins
+        return r:populate(args.count, config)
     end,
 }
 
