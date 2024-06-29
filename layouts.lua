1b21b8e4a1ab2343428ea0a7ef7d4689c7f7e8f6
thialfi17 <thialfi17@gmail.com>
2024-06-29 16:29:38 +0100

    Changes to layouts to add more features.
    Added the ability to shift the entire generated windows by X amount.
    This is useful for setting gaps against monitor edges or for creating
    regions to put floating windows.
    
    Also took this opportunity to fix a bug with the main_with_stack layout
    and to improve the commenting. It is now much easier to follow how the
    layout is created.

--- layouts.lua
+++ layouts.lua
@@ -18,7 +18,12 @@ local layouts = {
     ---@type Layout
     ---Stacks the windows on top of each other with no offset.
     monocle = function(args, config)
-        local r = Region:from_args("top", args):set_layout("fill")
+        local r = Region:from_args("top", args):set_layout("fill", {1, nil})
+
+        r.height = r.height - config.offset_top - config.offset_bottom
+        r.width = r.width - config.offset_left - config.offset_right
+        r.y = r.y + config.offset_top
+        r.x = r.x + config.offset_left
 
         if not (config.smart_gaps) then
             r:set_gaps(config.gaps)
@@ -31,7 +36,12 @@ local layouts = {
     ---Stacks the windows on top of each other with an offset
     ---so it is clearer that there are multiple windows.
     stack = function(args, config)
-        local r = Region:from_args("top", args):set_layout("stack")
+        local r = Region:from_args("top", args):set_layout("stack", {1, nil})
+
+        r.height = r.height - config.offset_top - config.offset_bottom
+        r.width = r.width - config.offset_left - config.offset_right
+        r.y = r.y + config.offset_top
+        r.x = r.x + config.offset_left
 
         r:set_gaps(config.gaps)
 
@@ -40,7 +50,12 @@ local layouts = {
 
     ---@type Layout
     col = function(args, config)
-        local r = Region:from_args("top", args):set_layout("rows")
+        local r = Region:from_args("top", args):set_layout("rows", {1, nil})
+
+        r.height = r.height - config.offset_top - config.offset_bottom
+        r.width = r.width - config.offset_left - config.offset_right
+        r.y = r.y + config.offset_top
+        r.x = r.x + config.offset_left
 
         r:set_gaps(config.gaps)
 
@@ -48,7 +63,12 @@ local layouts = {
     end,
 
     row = function(args, config)
-        local r = Region:from_args("top", args):set_layout("cols")
+        local r = Region:from_args("top", args):set_layout("cols", {1, nil})
+
+        r.height = r.height - config.offset_top - config.offset_bottom
+        r.width = r.width - config.offset_left - config.offset_right
+        r.y = r.y + config.offset_top
+        r.x = r.x + config.offset_left
 
         r:set_gaps(config.gaps)
 
@@ -58,7 +78,12 @@ local layouts = {
     ---@type Layout
     ---Arranges the windows in a grid.
     grid = function(args, config)
-        local r = Region:from_args("top", args):set_layout("grid")
+        local r = Region:from_args("top", args):set_layout("grid", {1, nil})
+
+        r.height = r.height - config.offset_top - config.offset_bottom
+        r.width = r.width - config.offset_left - config.offset_right
+        r.y = r.y + config.offset_top
+        r.x = r.x + config.offset_left
 
         if not (config.smart_gaps and args.count < 2) then
             r:set_gaps(config.gaps)
@@ -78,31 +103,59 @@ local layouts = {
         local secondary_sublayout = config.secondary_sublayout
         local tertiary_sublayout = config.tertiary_sublayout
 
-        local r = Region:from_args("top", args)
+        local r = Region:from_args("root", args)
+
+        r.height = r.height - config.offset_top - config.offset_bottom
+        r.width = r.width - config.offset_left - config.offset_right
+        r.y = r.y + config.offset_top
+        r.x = r.x + config.offset_left
 
         if not (config.smart_gaps and args.count < 2) then
             r:set_gaps(config.gaps)
         end
 
+        -- This is the layout that gets used if there aren't enough windows to
+        -- fill the "main" region and the "right" region.
+        r:set_layout(main_layout, {1, nil})
+
         local main_width = math.floor(main_ratio * r.width)
 
+        -- The "main" region should have no more or less than the specified
+        -- count. Any less uses the root "r" region and any more spill over
+        -- into the "right" region.
         local main = r:from("main", 0, 0, main_width, r.height):set_layout(main_layout, {main_count, main_count})
 
+        -- Force the "main" region to be filled before the "right" region.
         r:next_tier()
 
-        local sub = r:from("right", main_width, 0, r.width - main_width, r.height)
+        -- Create the overall region for windows to the right of the "main"
+        -- region. This will be used when there aren't enough windows to fill
+        -- the tertiary region or there is no secondary region.
+        local right = r:from("right", main_width, 0, r.width - main_width, r.height)
 
+        -- There is a secondary region
         if secondary_count ~= 0 then
-            sub:set_layout(secondary_sublayout, {1, nil})
+            -- So use the secondary layout if there aren't enough windows to
+            -- fill the tertiary region.
+            right:set_layout(secondary_sublayout, {1, nil})
 
-            local secondary_height = math.floor(secondary_ratio * sub.height)
-            local secondary = sub:from("secondary", 0, 0, sub.width, secondary_height):set_layout(secondary_sublayout, {secondary_count, secondary_count})
+            -- Regions are derived from "right" here to make the maths simpler
+            -- and restrict the coordinates to being inside the "right" region.
+            --
+            -- Because we use right:from these regions get put in the "right"
+            -- hierarchy. This means to force the secondary region to be filled
+            -- before the tertiary region we need to use the "right" hierarchy
+            -- as shown below.
+            local secondary_height = math.floor(secondary_ratio * right.height)
+            local secondary = right:from("secondary", 0, 0, right.width, secondary_height):set_layout(secondary_sublayout, {secondary_count, secondary_count})
 
-            sub:next_tier()
+            -- Fill the secondary region before attempting to fill the tertiary
+            -- region.
+            right:next_tier()
 
-            local remaining = sub:from("tertiary", 0, secondary_height, sub.width, sub.height - secondary_height):set_layout(tertiary_sublayout, {1, nil})
+            local remaining = right:from("tertiary", 0, secondary_height, right.width, right.height - secondary_height):set_layout(tertiary_sublayout, {1, nil})
         else
-            sub:set_layout(tertiary_sublayout, {1, nil})
+            right:set_layout(tertiary_sublayout, {1, nil})
         end
 
         return r:populate(args.count, config)
@@ -110,54 +163,59 @@ local layouts = {
 
     ---@type Layout
     ---@diagnostic disable: unused-local
-    centred = function(args, c)
-        local r = Region:from_args("top", args):set_layout(c.main_layout)
+    centred = function(args, config)
+        local r = Region:from_args("top", args):set_layout(config.main_layout, {1, nil})
 
-        if not (c.smart_gaps and args.count < 2) then
-            r:set_gaps(c.gaps)
+        r.height = r.height - config.offset_top - config.offset_bottom
+        r.width = r.width - config.offset_left - config.offset_right
+        r.y = r.y + config.offset_top
+        r.x = r.x + config.offset_left
+
+        if not (config.smart_gaps and args.count < 2) then
+            r:set_gaps(config.gaps)
         end
 
-        local main_offset = math.floor(r.width * (0.5 - c.main_ratio / 2))
-        local main_width =  math.floor(r.width * c.main_ratio)
+        local main_offset = math.floor(r.width * (0.5 - config.main_ratio / 2))
+        local main_width =  math.floor(r.width * config.main_ratio)
 
         -- If not enough windows are present to totally fill the main region and the two side regions,
         -- fill the main region with as much as we can while leaving two windows free to become the sides
-        local main_max = c.main_count
-        if args.count < c.main_count + 2 then
+        local main_max = config.main_count
+        if args.count < config.main_count + 2 then
             main_max = args.count - 2
         end
 
-        local main = r:from("main", main_offset, 0, main_width, r.height):set_layout(c.main_layout, {1, main_max})
+        local main = r:from("main", main_offset, 0, main_width, r.height):set_layout(config.main_layout, {1, main_max})
 
         r:next_tier()
 
         local left, right
 
-        if c.secondary_count == 0 then
-            left = r:from("left_t", 0, 0, main_offset, r.height):set_layout(c.tertiary_sublayout, {1, nil})
-            right = r:from("right_t", main_offset + main_width, 0, main_offset, r.height):set_layout(c.tertiary_sublayout, {1, nil})
+        if config.secondary_count == 0 then
+            left = r:from("left_t", 0, 0, main_offset, r.height):set_layout(config.tertiary_sublayout, {1, nil})
+            right = r:from("right_t", main_offset + main_width, 0, main_offset, r.height):set_layout(config.tertiary_sublayout, {1, nil})
         else
-            local secondary_height = math.floor(r.height * c.secondary_ratio)
+            local secondary_height = math.floor(r.height * config.secondary_ratio)
             local remaining_offset = secondary_height
             local remaining_height = r.height - remaining_offset
 
-            left = r:from("left", 0, 0, main_offset, r.height):set_layout(c.secondary_sublayout, {1, nil})
-            right = r:from("right", main_offset + main_width, 0, main_offset, r.height):set_layout(c.secondary_sublayout, {1, nil})
+            left = r:from("left", 0, 0, main_offset, r.height):set_layout(config.secondary_sublayout, {1, nil})
+            right = r:from("right", main_offset + main_width, 0, main_offset, r.height):set_layout(config.secondary_sublayout, {1, nil})
 
-            local left_top = left:from("left_top", 0, 0, main_offset, secondary_height):set_layout(c.secondary_sublayout, {c.secondary_count, c.secondary_count})
+            local left_top = left:from("left_top", 0, 0, main_offset, secondary_height):set_layout(config.secondary_sublayout, {config.secondary_count, config.secondary_count})
 
             left:next_tier()
 
-            local left_btm = left:from("left_bot", 0, secondary_height, main_offset, remaining_height):set_layout(c.tertiary_sublayout)
+            local left_btm = left:from("left_bot", 0, secondary_height, main_offset, remaining_height):set_layout(config.tertiary_sublayout, {1, nil})
 
-            local right_top = right:from("right_top", 0, 0, main_offset, secondary_height):set_layout(c.secondary_sublayout, {c.secondary_count, c.secondary_count})
+            local right_top = right:from("right_top", 0, 0, main_offset, secondary_height):set_layout(config.secondary_sublayout, {config.secondary_count, config.secondary_count})
 
             right:next_tier()
 
-            local right_btm = right:from("right_bot", 0, secondary_height, main_offset, remaining_height):set_layout(c.tertiary_sublayout)
+            local right_btm = right:from("right_bot", 0, secondary_height, main_offset, remaining_height):set_layout(config.tertiary_sublayout, {1, nil})
         end
 
-        return r:populate(args.count, c)
+        return r:populate(args.count, config)
     end,
 }
 
--- user_settings.lua
+++ user_settings.lua
@@ -1,31 +1,63 @@
 ----------------------
 --   User Options   --
 ----------------------
+-- These are global options that affect how the layouts are generated. All
+-- options that are used in ANY layout should have a value set below.
+--
+-- NOTE: Not all options are used for all layouts.
+--
+-- It is recommended to install the luals LSP to provide signature help for
+-- built-in functions. If you can't install the LSP then either refer to any
+-- documentation or read through the source code. The signature for the
+-- set_global function has been provided below:
+--
+--   set_global( "option", VALUE, MIN, MAX )
+--
+-- If you have modified any of the layout code you may find adding more debug
+-- messages useful. To set the log level to include debug messages uncomment the
+-- following line:
+--
+-- require("backend.utils").set_log_level(DEBUG)
+--
+-- NOTE: There aren't many debug messages included by default at the moment.
 
 set_global("per-layout-config", true)
 
-set_global("main_ratio",      0.6, 0.1, 0.9)
-set_global("main_count",      1,   1,   nil) -- Doesn't make sense not to have at least one main window
-set_global("main_layout", "grid")
-set_global("secondary_ratio", 0.6, 0.1, 0.9)
-set_global("secondary_count", 1,   0,   nil)
+set_global("main_ratio",        0.6, 0.1, 0.9)
+set_global("main_count",        1,   1,   nil) -- Doesn't make sense not to have at least one main window
+set_global("main_layout",       "grid")
 
-set_global("secondary_sublayout", "grid")
-set_global("tertiary_sublayout",  "stack")
+set_global("secondary_ratio",       0.6, 0.1, 0.9)
+set_global("secondary_count",       1,   0,   nil)
+set_global("secondary_sublayout",   "grid")
 
-set_global("gaps",            4,   0,   nil) -- To disable set to 0
-set_global("smart_gaps", true)
+set_global("tertiary_sublayout",    "stack")
+
+set_global("gaps", 4,   0,   nil) -- To disable set to 0
+set_global("smart_gaps",   false)
 
 set_global("layout", "main_with_stack")
+
+-- Move the entire region of tiled windows away from the edges.
+-- Can be useful for creating a region to put floating windows or for
+-- edge-specific gaps.
+set_global("offset_top",    0, 0, nil)
+set_global("offset_bottom", 0, 0, nil)
+set_global("offset_left",   0, 0, nil)
+set_global("offset_right",  0, 0, nil)
 
 -----------------------
 -- Sublayout Options --
 -----------------------
+-- No different from the other global options except for where they are used.
 
+-- Maximum offset of `stack` sublayout
 set_global("max_offset",    30,   0, nil)
+-- Target ratio the grid sublayout aims for
 set_global("grid_ratio",  16/9, 1/3, 3/1)
-
 
 -----------------------
 --    Tag Options    --
 -----------------------
+-- TODO: Add configuration examples for tag specific options.
+-- TODO: Configuration for scratch tag.
